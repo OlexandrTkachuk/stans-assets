30c95f4d35472a9765e99426d6822d63cab7a370
OlexandrTkachuk <106085458+OlexandrTkachuk@users.noreply.github.com>
2023-06-06 15:52:41 +0000

    Deploying to gh-pages from @ OlexandrTkachuk/stans-assets@130a6dfcfa86ebc4f6641e3436c6910f40b58622 🚀

--- static/js/248.9d090d3e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[248],{4548:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:"Services"})}}}]);
-//# sourceMappingURL=248.9d090d3e.chunk.js.map
